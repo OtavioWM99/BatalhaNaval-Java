f7c0b6dd54e25bc23b647522bc7a8bcf1504458b
Otavio WM <otaviowerlang99@gmil.com>
2025-05-06 20:41:15 -0300

    Commit inicial

---
+++ src/BatalhaNaval/BatalhaNaval.java
@@ -0,0 +1,15 @@
+package BatalhaNaval;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BatalhaNaval {
+    static final int MAPA = 10;
+    static final char AGUA = '~';
+    static final char NAVIO = 'N';
+    static final char TIRO_NA_AGUA = 'O';
+    static final char TIRO_NO_NAVIO = 'X';
+
+    static Scanner ler = new Scanner(System.in);
+    static Random aleatorio = new Random();
+}
